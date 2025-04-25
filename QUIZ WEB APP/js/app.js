f6ac17d6a02412c33c316bdb62b71ecff3f617f5
Co-dick <166649163+Co-dick@users.noreply.github.com>
2025-04-26 02:10:01 +0530

    Add files via upload

---
+++ QUIZ WEB APP/js/app.js
@@ -0,0 +1,45 @@
+const questions = [
+    {
+        'ques': 'what is the full form of "HTML"?',
+        'a': 'hyper text makeup language',
+        'b': 'hyper text markup language',
+        'c': 'hyper text making language',
+        'd': 'hard technology making language',
+        'correct': 'b'
+    },
+    {
+        'ques': 'What is the other name used for computer programs and information? ',
+        'a': 'output',
+        'b': 'process',
+        'c': 'software',
+        'd': 'CPU',
+        'correct': 'c'
+    },
+    {
+        numb:5,
+        question:'The part of a computer that allows the user to view information on a screen is called:',
+        answer:"monitor",
+        option:[        
+         'Mouse',
+         'RAM',
+         'Motherboard',
+         'Moniter',
+        ]
+    },
+    {
+        'ques': ' Name the part that sends signals to other parts of the computer and tells them what to do.',
+        'a': 'CPU',
+        'b': 'Motherboard',
+        'c': 'Icon',
+        'd': 'Hard drive',
+        'correct': 'b'
+    },
+    {
+        'ques': 'This memory is for short-term storage and is lost when the computer is turned off.',
+        'a': 'Icon',
+        'b': 'CPU',
+        'c': 'Hard Drive',
+        'd': 'RAM',
+        'correct': 'd'
+    }
+];
